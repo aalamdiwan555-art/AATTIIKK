2784df8e7d8b58e844071567f997c394ca08f81d
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-08-31 19:32:17 +0530

    Add app/src/main/java/com/topperg/MainActivity.kt

---
+++ app/src/main/java/com/topperg/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.topperg
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.topperg.ui.navigation.TopperGNavGraph
+import com.topperg.ui.theme.TopperGTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TopperGTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    TopperGNavGraph(navController = navController)
+                }
+            }
+        }
+    }
+}
