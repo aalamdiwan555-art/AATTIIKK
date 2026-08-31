6e4bf700c6c0bc8127d6a953d6c7151d2ce3e8f3
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-08-31 19:33:55 +0530

    Add app/src/main/java/com/topperg/ui/theme/Color.kt

---
+++ app/src/main/java/com/topperg/ui/theme/Color.kt
@@ -0,0 +1,51 @@
+package com.topperg.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary
+val Primary = Color(0xFF2A2F6B)
+val PrimaryDark = Color(0xFF1E224D)
+val PrimaryLight = Color(0xFF4A5090)
+
+// Accent
+val Accent = Color(0xFFF4A100)
+val AccentLight = Color(0xFFFFB833)
+
+// Success / Error
+val Success = Color(0xFF2E9E5B)
+val Error = Color(0xFFC6553D)
+
+// Surfaces - Light
+val SurfaceLight = Color(0xFFFAF8F3)
+val SurfaceLightVariant = Color(0xFFF0EDE6)
+val BackgroundLight = Color(0xFFFAF8F3)
+
+// Surfaces - Dark
+val SurfaceDark = Color(0xFF171A21)
+val SurfaceDarkVariant = Color(0xFF1E222B)
+val BackgroundDark = Color(0xFF171A21)
+
+// Text - Light
+val TextPrimaryLight = Color(0xFF1C1C1E)
+val TextSecondaryLight = Color(0xFF6B6B6B)
+val TextTertiaryLight = Color(0xFF9E9E9E)
+
+// Text - Dark
+val TextPrimaryDark = Color(0xFFEDEDED)
+val TextSecondaryDark = Color(0xFFA0A0A0)
+val TextTertiaryDark = Color(0xFF707070)
+
+// Common
+val DividerLight = Color(0xFFE0DDD5)
+val DividerDark = Color(0xFF2D3139)
+val OverlayLight = Color(0x1F000000)
+val OverlayDark = Color(0x4D000000)
+
+// Subject colors
+val SubjectMath = Color(0xFF4A90D9)
+val SubjectScience = Color(0xFF5CB85C)
+val SubjectSocial = Color(0xFFD4A373)
+val SubjectEnglish = Color(0xFF9B59B6)
+val SubjectHindi = Color(0xFFE74C3C)
+val SubjectSanskrit = Color(0xFF1ABC9C)
+val SubjectComputer = Color(0xFF34495E)
