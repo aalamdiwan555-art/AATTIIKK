388a7d5d944e4008b47e20bfa92c7226e1a63776
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-09-01 09:36:15 +0530

    Import downloads list arrangement

--- app/src/main/java/com/topperg/ui/screens/settings/DownloadsManagerScreen.kt
+++ app/src/main/java/com/topperg/ui/screens/settings/DownloadsManagerScreen.kt
@@ -1,5 +1,6 @@
 package com.topperg.ui.screens.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
